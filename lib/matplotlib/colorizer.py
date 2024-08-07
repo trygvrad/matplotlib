1fb5dd808b1367aaad66da29ebeab3b722f6aa5e
Trygve Magnus Ræder <trygvrad@gmail.com>
2024-08-07 15:16:40 +0200

    MultiNorm class

--- lib/matplotlib/colorizer.py
+++ lib/matplotlib/colorizer.py
@@ -17,7 +17,6 @@ normalization and a colormap.
 
 import numpy as np
 from numpy import ma
-import functools
 from matplotlib import _api, colors, cbook, scale, cm
 
 
@@ -78,7 +77,7 @@ class Colorizer():
                     "Invalid norm str name; the following values are "
                     f"supported: {', '.join(scale._scale_mapping)}"
                 ) from None
-            norm = _auto_norm_from_scale(scale_cls)()
+            norm = colors._auto_norm_from_scale(scale_cls)()
 
         if norm is self.norm:
             # We aren't updating anything
@@ -453,31 +452,3 @@ class ColorizerShim:
     @colorbar.setter
     def colorbar(self, colorbar):
         self.colorizer.colorbar = colorbar
-
-
-def _auto_norm_from_scale(scale_cls):
-    """
-    Automatically generate a norm class from *scale_cls*.
-
-    This differs from `.colors.make_norm_from_scale` in the following points:
-
-    - This function is not a class decorator, but directly returns a norm class
-      (as if decorating `.Normalize`).
-    - The scale is automatically constructed with ``nonpositive="mask"``, if it
-      supports such a parameter, to work around the difference in defaults
-      between standard scales (which use "clip") and norms (which use "mask").
-
-    Note that ``make_norm_from_scale`` caches the generated norm classes
-    (not the instances) and reuses them for later calls.  For example,
-    ``type(_auto_norm_from_scale("log")) == LogNorm``.
-    """
-    # Actually try to construct an instance, to verify whether
-    # ``nonpositive="mask"`` is supported.
-    try:
-        norm = colors.make_norm_from_scale(
-            functools.partial(scale_cls, nonpositive="mask"))(
-            colors.Normalize)()
-    except TypeError:
-        norm = colors.make_norm_from_scale(scale_cls)(
-            colors.Normalize)()
-    return type(norm)
