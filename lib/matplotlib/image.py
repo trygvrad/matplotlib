Fix additional misalignments due to numerical precision
There are multiple instances where a value that would ideally be exactly
at a half display pixel might instead be one floating-point tick away in
the wrong direction.  Here fudge amounts are added so that the rounding
occurs in the desired direction.  An analogous situation occurs for
truncation to a whole pixel.  The amount of 1e-8 is chosen because it
covers 1e-13 relative error for 100,000 pixels.

lib/matplotlib/image.py
@@ -410,8 +410,12 @@ class _ImageBase(mcolorizer.ColorizingArtist):
         magnified_extents = clipped_bbox.extents * magnification
         if ((not unsampled) and round_to_pixel_border):
             # Round to the nearest output pixel
-            x0, x1 = np.floor(magnified_extents[0::2] + 0.5)  # round half up
-            y0, y1 = np.ceil(magnified_extents[1::2] - 0.5)  # round half down
+            # Add a tiny fudge amount to account for numerical precision loss
+            # on the two sides away from the Agg anchor point (x0, y1)
+            x0 = np.floor(magnified_extents[0] + 0.5)  # round half up
+            y0 = np.ceil(magnified_extents[1] - 0.5 - 1e-8)  # round half down
+            x1 = np.floor(magnified_extents[2] + 0.5 + 1e-8)  # round half up
+            y1 = np.ceil(magnified_extents[3] - 0.5)  # round half down
             magnified_bbox = Bbox.from_extents([x0, y0, x1, y1])
         else:
             magnified_bbox = Bbox.from_extents(magnified_extents)