de8e26771cb53344939d39be2825c18fc2b7180f
Jaylon <jaylonw2@illinois.edu>
2026-04-11 20:45:10 +0800

    Remove outdated FIXME comment

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -1032,7 +1032,7 @@ def delete_masked_points(*args):
                 mask = np.isfinite(xd)
                 if isinstance(mask, np.ndarray):
                     masks.append(mask)
-            except (TypeError, ValueError):  # Fixme: put in tuple of possible exceptions?
+            except (TypeError, ValueError):
                 pass
     if len(masks):
         mask = np.logical_and.reduce(masks)
