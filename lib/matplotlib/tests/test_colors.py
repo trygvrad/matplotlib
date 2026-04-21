b07211fda7715b9409a7785816162a7102e49a64
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2026-04-21 16:08:53 -0400

    Add suggestions to more lookup errors (#31514)
    * Tweak wording of suggestion message from getitem_checked
    
    No need for 'one of' if there's only one option, add a colon to
    match the syntax of Python's own suggestion message, and remove the
    trailing space if there's no suggestion.
    
    * Add suggestions to ColorSequenceRegistry lookup errors
    
    * Remove _print_supported_values parameter from _api.check_in_list
    
    The only usage was removed in 8cde5a816715061bcabd98eae2e87cb5f36805c7.
    
    * Harmonize error messages from _api.check_in_list and _api.getitem_checked
    
    This also means that the former now gains suggestions.
    
    * Add suggestions to ProjectionRegistry lookup errors
    
    * Add suggestions to MovieWriterRegistry lookup errors
    
    * Add suggestions to Colorizer cmap lookup errors

--- lib/matplotlib/tests/test_colors.py
+++ lib/matplotlib/tests/test_colors.py
@@ -1882,10 +1882,15 @@ def test_close_error_name():
     with pytest.raises(
         KeyError,
         match=(
-            "'grays' is not a valid value for colormap. "
-            "Did you mean one of ['gray', 'Grays', 'gray_r']?"
-        )):
+            r"'grays' is not a valid value for colormap\. "
+            r"Did you mean one of: 'gray', 'Grays', 'gray_r'\?")):
         matplotlib.colormaps["grays"]
+    with pytest.raises(
+        KeyError,
+        match=(
+            "'set' is not a valid value for sequence_name. "
+            "Did you mean one of: 'Set3', 'Set2', 'Set1'?")):
+        matplotlib.color_sequences["set"]
 
 
 def test_multi_norm_creation():
